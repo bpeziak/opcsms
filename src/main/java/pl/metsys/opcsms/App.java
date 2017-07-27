d604a94403794ade6a383cde8eb761bc34c89e81
bpeziak <b.peziak@gmail.com>
2017-07-27 22:13:23 +0200

    Application skeleton generated by Maven

---
+++ src/main/java/pl/metsys/opcsms/App.java
@@ -0,0 +1,14 @@
+package pl.metsys.opcsms;
+
+/**
+ * Hello world!
+ * pl.metsys.opcsms.gui ; *.opc ; *.serial
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
